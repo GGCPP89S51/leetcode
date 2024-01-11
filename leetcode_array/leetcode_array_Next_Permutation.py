b9e0c675fe926e8a1f3e70aa6fdcc0993b049a51
GGCPP89S51 <92510306+GGCPP89S51@users.noreply.github.com>
2024-01-11 12:15:05 +0800

    Update leetcode_array_Next_Permutation.py

---
+++ leetcode_array/leetcode_array_Next_Permutation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        # 步驟2
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # 步驟3
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
